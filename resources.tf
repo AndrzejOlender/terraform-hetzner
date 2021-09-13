2d84de563d9fa72368dfad680a4b668f5db2fa32
Andrzej Olender <andrzej@olender.me>
2021-09-13 16:11:36 +0200

    change server type

--- resources.tf
+++ resources.tf
@@ -1,7 +1,7 @@
 # hcloud
 resource "hcloud_server" "node1" {
   name        = "node1"
-  server_type = "cx31"
+  server_type = "cx21"
   datacenter  = data.hcloud_datacenter.ds.name
   image       = "ubuntu-20.04"
   ssh_keys    = ["nuc"]
@@ -9,7 +9,7 @@ resource "hcloud_server" "node1" {
 
 resource "hcloud_server" "node2" {
   name        = "node2"
-  server_type = "cx31"
+  server_type = "cx21"
   datacenter  = data.hcloud_datacenter.ds.name
   image       = "ubuntu-20.04"
   ssh_keys    = ["nuc"]
@@ -17,7 +17,7 @@ resource "hcloud_server" "node2" {
 
 resource "hcloud_server" "node3" {
   name        = "node3"
-  server_type = "cx31"
+  server_type = "cx21"
   datacenter  = data.hcloud_datacenter.ds.name
   image       = "ubuntu-20.04"
   ssh_keys    = ["nuc"]
