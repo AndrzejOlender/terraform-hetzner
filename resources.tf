5c4f6b1c2d333b8a89d1644b6579cefff5c1da88
Andrzej Olender <andrzej@olender.me>
2021-09-15 23:46:52 +0200

    test

--- resources.tf
+++ resources.tf
@@ -15,10 +15,19 @@ resource "hcloud_server" "node2" {
   ssh_keys    = ["nuc"]
 }
 
+# resource "hcloud_server" "node3" {
+#   name        = "node3"
+#   server_type = "cx21"
+#   datacenter  = data.hcloud_datacenter.ds.name
+#   image       = "ubuntu-20.04"
+#   ssh_keys    = ["nuc"]
+# }
+
 resource "hcloud_server" "node3" {
   name        = "node3"
   server_type = "cx21"
   datacenter  = data.hcloud_datacenter.ds.name
+  id          = "48035117"
   image       = "ubuntu-20.04"
   ssh_keys    = ["nuc"]
 }
