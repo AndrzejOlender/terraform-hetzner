275dc7a1c1c6bea0ea15613c1ccc3fe9846f804e
Andrzej Olender <andrzej@olender.me>
2021-09-16 23:43:57 +0200

    disable network 1

--- network.tf
+++ network.tf
@@ -10,11 +10,11 @@ resource "hcloud_network_subnet" "k8s" {
   ip_range     = "10.0.1.0/24"
 }
 
-resource "hcloud_server_network" "node1" {
-  server_id  = hcloud_server.node1.id
-  network_id = hcloud_network.k8s.id
-  ip         = "10.0.1.11"
-}
+# resource "hcloud_server_network" "node1" {
+#   server_id  = hcloud_server.node1.id
+#   network_id = hcloud_network.k8s.id
+#   ip         = "10.0.1.11"
+# }
 
 resource "hcloud_server_network" "node2" {
   server_id  = hcloud_server.node2.id
