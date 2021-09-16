312cbfc4d2125e04f4590bc55797aceb34af27cb
Andrzej Olender <andrzej@olender.me>
2021-09-16 20:55:51 +0200

    Update resources.tf

--- resources.tf
+++ resources.tf
@@ -1,4 +1,7 @@
 # hcloud
+data "hcloud_image" "k3os" {
+  id = "48035117"
+}
 resource "hcloud_server" "node1" {
   name        = "node1"
   server_type = "cx21"
@@ -23,3 +26,13 @@ resource "hcloud_server" "node3" {
   ssh_keys    = ["nuc"]
 }
 
+resource "hcloud_server" "main" {
+  image = data.hcloud_image.image_1.name
+}
+
+resource "hcloud_server" "k3os" {
+  name        = "k3os"
+  server_type = "cx21"
+  datacenter  = data.hcloud_datacenter.ds.name
+  ssh_keys    = ["nuc"]
+}
