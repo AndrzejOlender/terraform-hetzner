b69115601c7b2ffad566b25949dcdbbab33e9d14
Andrzej Olender <andrzej@olender.me>
2021-09-16 21:44:13 +0200

    add k3os image

--- resources.tf
+++ resources.tf
@@ -1,8 +1,12 @@
+data "hcloud_image" "k3os" {
+  id = "48035117"
+}
+
 resource "hcloud_server" "node1" {
   name        = "node1"
   server_type = "cx21"
   datacenter  = data.hcloud_datacenter.ds.name
-  image       = "ubuntu-20.04"
+  image       = data.hcloud_image.k3os.id
   ssh_keys    = ["nuc"]
 }
 
@@ -10,7 +14,7 @@ resource "hcloud_server" "node2" {
   name        = "node2"
   server_type = "cx21"
   datacenter  = data.hcloud_datacenter.ds.name
-  image       = "ubuntu-20.04"
+  image       = data.hcloud_image.k3os.id
   ssh_keys    = ["nuc"]
 }
 
@@ -18,18 +22,6 @@ resource "hcloud_server" "node3" {
   name        = "node3"
   server_type = "cx21"
   datacenter  = data.hcloud_datacenter.ds.name
-  image       = "ubuntu-20.04"
-  ssh_keys    = ["nuc"]
-}
-
-data "hcloud_image" "image_1" {
-  id = "48035117"
-}
-
-resource "hcloud_server" "main" {
-  name        = "main"
-  server_type = "cx21"
-  datacenter  = data.hcloud_datacenter.ds.name
-  image = data.hcloud_image.image_1.id
+  image       = data.hcloud_image.k3os.id
   ssh_keys    = ["nuc"]
 }
