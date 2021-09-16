60503f2c6d05a017346d09811358ce6218dbb777
Andrzej Olender <andrzej@olender.me>
2021-09-16 21:54:24 +0200

    add mac ssh key

--- ssh.tf
+++ ssh.tf
@@ -2,7 +2,12 @@ variable "ssh" {
   type = string
 }
 
-resource "hcloud_ssh_key" "default" {
-   name       = "nuc"
-   public_key = var.ssh
- } 
+resource "hcloud_ssh_key" "nuc" {
+  name       = "nuc"
+  public_key = var.ssh
+} 
+
+resource "hcloud_ssh_key" "mac" {
+  name       = "mac"
+  public_key = var.ssh-mac
+} 
