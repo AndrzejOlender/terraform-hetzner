b709028dfe397a30ebc379a27216e37abc05318f
Andrzej Olender <andrzej@olender.me>
2021-09-16 22:20:13 +0200

    disable nodes

--- resources.tf
+++ resources.tf
@@ -1,27 +1,27 @@
-data "hcloud_image" "k3os" {
-  id = "48035117"
-}
+# data "hcloud_image" "k3os" {
+#   id = "48035117"
+# }
 
-resource "hcloud_server" "node1" {
-  name        = "node1"
-  server_type = "cx21"
-  datacenter  = data.hcloud_datacenter.ds.name
-  image       = data.hcloud_image.k3os.id
-  ssh_keys    = ["nuc"]
-}
+# resource "hcloud_server" "node1" {
+#   name        = "node1"
+#   server_type = "cx21"
+#   datacenter  = data.hcloud_datacenter.ds.name
+#   image       = data.hcloud_image.k3os.id
+#   ssh_keys    = ["nuc"]
+# }
 
-resource "hcloud_server" "node2" {
-  name        = "node2"
-  server_type = "cx21"
-  datacenter  = data.hcloud_datacenter.ds.name
-  image       = data.hcloud_image.k3os.id
-  ssh_keys    = ["nuc"]
-}
+# resource "hcloud_server" "node2" {
+#   name        = "node2"
+#   server_type = "cx21"
+#   datacenter  = data.hcloud_datacenter.ds.name
+#   image       = data.hcloud_image.k3os.id
+#   ssh_keys    = ["nuc"]
+# }
 
-resource "hcloud_server" "node3" {
-  name        = "node3"
-  server_type = "cx21"
-  datacenter  = data.hcloud_datacenter.ds.name
-  image       = data.hcloud_image.k3os.id
-  ssh_keys    = ["nuc"]
-}
+# resource "hcloud_server" "node3" {
+#   name        = "node3"
+#   server_type = "cx21"
+#   datacenter  = data.hcloud_datacenter.ds.name
+#   image       = data.hcloud_image.k3os.id
+#   ssh_keys    = ["nuc"]
+# }
