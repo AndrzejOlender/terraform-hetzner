df2b62da03c82c29621a9cc49ade527e24949527
Andrzej Olender <andrzej@olender.me>
2021-09-16 23:45:50 +0200

    add node1

--- resources.tf
+++ resources.tf
@@ -2,16 +2,16 @@
 #   id = "15512617"
 # }
 
-# resource "hcloud_server" "node1" {
-#   name        = "node1"
-#   server_type = "cx21"
-#   datacenter  = data.hcloud_datacenter.ds.name
-#   image       = "ubuntu-20.04"
-#   ssh_keys    = ["nuc","mac"]
-#   provisioner "local-exec" {
-#     command = "curl -sLS https://get.k3sup.dev | sh; sudo install k3sup /usr/local/bin/"
-#   }
-# }
+resource "hcloud_server" "node1" {
+  name        = "node1"
+  server_type = "cx21"
+  datacenter  = data.hcloud_datacenter.ds.name
+  image       = "ubuntu-20.04"
+  ssh_keys    = ["nuc","mac"]
+  provisioner "local-exec" {
+    command = "curl -sLS https://get.k3sup.dev | sh; sudo install k3sup /usr/local/bin/"
+  }
+}
 
 resource "hcloud_server" "node2" {
   name        = "node2"
