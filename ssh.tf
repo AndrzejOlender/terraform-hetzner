bdfbce48338898208631669e170e017c40ca57ea
Andrzej Olender <andrzej@olender.me>
2021-09-16 21:58:46 +0200

    disable ssh nuc key

--- ssh.tf
+++ ssh.tf
@@ -1,15 +1,15 @@
-variable "ssh" {
-  type = string
-}
+# variable "ssh" {
+#   type = string
+# }
 
 variable "ssh-mac" {
   type = string
 }
 
-resource "hcloud_ssh_key" "nuc" {
-  name       = "nuc"
-  public_key = var.ssh
-} 
+# resource "hcloud_ssh_key" "nuc" {
+#   name       = "nuc"
+#   public_key = var.ssh
+# } 
 
 resource "hcloud_ssh_key" "mac" {
   name       = "mac"
