609431084e26f8c148185e1001e484da2bebfb15
Andrzej Olender <andrzej@olender.me>
2021-09-16 22:57:20 +0200

    change image

--- resources.tf
+++ resources.tf
@@ -1,12 +1,12 @@
-data "hcloud_image" "k3os" {
-  id = "48035117"
+data "hcloud_image" "ubuntu" {
+  id = "15512617"
 }
 
 resource "hcloud_server" "node1" {
   name        = "node1"
   server_type = "cx21"
   datacenter  = data.hcloud_datacenter.ds.name
-  image       = data.hcloud_image.k3os.id
+  image       = data.hcloud_image.ubuntu.id
   ssh_keys    = ["nuc","mac"]
 }
 
@@ -14,7 +14,7 @@ resource "hcloud_server" "node2" {
   name        = "node2"
   server_type = "cx21"
   datacenter  = data.hcloud_datacenter.ds.name
-  image       = data.hcloud_image.k3os.id
+  image       = data.hcloud_image.ubuntu.id
   ssh_keys    = ["nuc","mac"]
 }
 
@@ -22,6 +22,6 @@ resource "hcloud_server" "node3" {
   name        = "node3"
   server_type = "cx21"
   datacenter  = data.hcloud_datacenter.ds.name
-  image       = data.hcloud_image.k3os.id
+  image       = data.hcloud_image.ubuntu.id
   ssh_keys    = ["nuc","mac"]
 }
