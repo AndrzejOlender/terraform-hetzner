a7cd9e278486eddfaa2f240effb03fd8fb33e82c
Andrzej Olender <andrzej@olender.me>
2021-09-16 22:33:38 +0200

    small corrects

--- resources.tf
+++ resources.tf
@@ -7,7 +7,7 @@ resource "hcloud_server" "node1" {
   server_type = "cx21"
   datacenter  = data.hcloud_datacenter.ds.name
   image       = data.hcloud_image.k3os.id
-  ssh_keys    = [nuc,mac]
+  ssh_keys    = ["nuc,mac"]
 }
 
 resource "hcloud_server" "node2" {
@@ -15,7 +15,7 @@ resource "hcloud_server" "node2" {
   server_type = "cx21"
   datacenter  = data.hcloud_datacenter.ds.name
   image       = data.hcloud_image.k3os.id
-  ssh_keys    = [nuc,mac]
+  ssh_keys    = ["nuc,mac"]
 }
 
 resource "hcloud_server" "node3" {
@@ -23,5 +23,5 @@ resource "hcloud_server" "node3" {
   server_type = "cx21"
   datacenter  = data.hcloud_datacenter.ds.name
   image       = data.hcloud_image.k3os.id
-  ssh_keys    = [nuc,mac]
+  ssh_keys    = ["nuc,mac"]
 }
