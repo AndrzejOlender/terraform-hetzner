b00bc33aae82f48d55aee743be3cbf3d14c059e1
Andrzej Olender <andrzej@olender.me>
2021-09-17 20:17:13 +0200

    test

--- resources.tf
+++ resources.tf
@@ -2,24 +2,24 @@
 #   id = "15512617"
 # }
 
-resource "hcloud_server" "node1" {
-  name        = "node1"
-  server_type = "cx21"
-  datacenter  = data.hcloud_datacenter.ds.name
-  image       = "ubuntu-20.04"
-  ssh_keys    = ["nuc","mac"]
-  provisioner "remote-exec" {
-    inline = [
-      "echo abc > /abcd.txt"
-    ]
-    connection {
-      type        = "ssh"
-      user        = "root"
-      host        = "168.119.224.111"
-      password    = "var.node-password"
-    }
-  }
-}
+# resource "hcloud_server" "node1" {
+#   name        = "node1"
+#   server_type = "cx21"
+#   datacenter  = data.hcloud_datacenter.ds.name
+#   image       = "ubuntu-20.04"
+#   ssh_keys    = ["nuc","mac"]
+#   provisioner "remote-exec" {
+#     inline = [
+#       "echo abc > /abcd.txt"
+#     ]
+#     connection {
+#       type        = "ssh"
+#       user        = "root"
+#       host        = "168.119.224.111"
+#       password    = "var.node-password"
+#     }
+#   }
+# }
 
 resource "hcloud_server" "node2" {
   name        = "node2"
@@ -29,21 +29,21 @@ resource "hcloud_server" "node2" {
   ssh_keys    = ["nuc","mac"]
 }
 
-resource "hcloud_server" "node3" {
-  name        = "node3"
-  server_type = "cx21"
-  datacenter  = data.hcloud_datacenter.ds.name
-  image       = "ubuntu-20.04"
-  ssh_keys    = ["nuc","mac"]
-  provisioner "remote-exec" {
-    inline = [
-      "echo abc > /abcd.txt"
-    ]
-    connection {
-      type        = "ssh"
-      user        = "root"
-      host        = "168.119.224.111"
-      password    = "var.node-password"
-    }
-  }
-}
+# resource "hcloud_server" "node3" {
+#   name        = "node3"
+#   server_type = "cx21"
+#   datacenter  = data.hcloud_datacenter.ds.name
+#   image       = "ubuntu-20.04"
+#   ssh_keys    = ["nuc","mac"]
+#   provisioner "remote-exec" {
+#     inline = [
+#       "echo abc > /abcd.txt"
+#     ]
+#     connection {
+#       type        = "ssh"
+#       user        = "root"
+#       host        = "168.119.224.111"
+#       password    = "var.node-password"
+#     }
+#   }
+# }
