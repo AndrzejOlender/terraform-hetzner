205d4d70e20857e5ea51b7a79f1a354af3664927
Andrzej Olender <andrzej@olender.me>
2021-09-18 22:30:38 +0200

    add resources

--- resources.tf
+++ resources.tf
@@ -1,21 +1,21 @@
-# resource "hcloud_server" "node1" {
-#   name        = "node1"
-#   server_type = "cx21"
-#   datacenter  = data.hcloud_datacenter.ds.name
-#   image       = "ubuntu-20.04"
-#   ssh_keys    = ["nuc","mac"]
-#   provisioner "remote-exec" {
-#     inline = [
-#       "echo ${var.ssh_user} > /abcd.txt"
-#     ]
-#     connection {
-#       type        = "ssh"
-#       host        = "168.119.224.111"
-#       user        = var.ssh_user
-#       password    = var.ssh_password
-#     }
-#   }
-# }
+resource "hcloud_server" "node1" {
+  name        = "node1"
+  server_type = "cx21"
+  datacenter  = data.hcloud_datacenter.ds.name
+  image       = "ubuntu-20.04"
+  ssh_keys    = ["nuc","mac"]
+  provisioner "remote-exec" {
+    inline = [
+      "echo ${var.ssh_user} > /abcd.txt"
+    ]
+    connection {
+      type        = "ssh"
+      host        = "168.119.224.111"
+      user        = var.ssh_user
+      password    = var.ssh_password
+    }
+  }
+}
 
 resource "hcloud_server" "node2" {
   name        = "node2"
@@ -36,21 +36,21 @@ resource "hcloud_server" "node2" {
   }
 }
 
-# resource "hcloud_server" "node3" {
-#   name        = "node3"
-#   server_type = "cx21"
-#   datacenter  = data.hcloud_datacenter.ds.name
-#   image       = "ubuntu-20.04"
-#   ssh_keys    = ["nuc","mac"]
-#   provisioner "remote-exec" {
-#     inline = [
-#       "echo abc > /abcd.txt"
-#     ]
-#     connection {
-#       type        = "ssh"
-#       host        = "168.119.224.111"
-#       user        = var.ssh_user
-#       password    = var.ssh_password
-#     }
-#   }
-# }
+resource "hcloud_server" "node3" {
+  name        = "node3"
+  server_type = "cx21"
+  datacenter  = data.hcloud_datacenter.ds.name
+  image       = "ubuntu-20.04"
+  ssh_keys    = ["nuc","mac"]
+  provisioner "remote-exec" {
+    inline = [
+      "echo abc > /abcd.txt"
+    ]
+    connection {
+      type        = "ssh"
+      host        = "168.119.224.111"
+      user        = var.ssh_user
+      password    = var.ssh_password
+    }
+  }
+}
