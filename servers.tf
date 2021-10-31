2690e5a591622a18dad49d19770f5b81eb2ca420
Andrzej Olender <andrzej@olender.me>
2021-10-31 22:15:15 +0100

    new modules test

--- servers.tf
+++ servers.tf
@@ -1,29 +1,108 @@
-# cluster 1
-module "cluster" {
-  source       = "cicdteam/k3s/hcloud"
-  version      = "0.1.2"
-  hcloud_token = var.hcloud_token
-  ssh_keys     = ["nuc", "mac"]
-  datacenter   = "nbg1-dc3"
-  cluster_name = "k8s"
-  image        = "ubuntu-20.04"
-  k3s_channel  = "v1.21"
-  master_type  = "cx21"
-  node_groups  = {
-    "cx21" = 2
-  }
+provider "hcloud" {
+  token = var.hcloud_token
 }
 
-# network output
+resource "hcloud_network" "private" {
+  name     = var.cluster_name
+  ip_range = "10.0.0.0/8"
+}
+
+resource "hcloud_network_subnet" "subnet" {
+  network_id   = hcloud_network.private.id
+  type         = "cloud"
+  network_zone = "eu-central"
+  ip_range     = "10.0.0.0/24"
+}
+
+resource "random_string" "k3s_token" {
+  length  = 48
+  upper   = false
+  special = false
+}
+
+module "master" {
+  source = "./modules/master"
+
+  cluster_name = var.cluster_name
+  datacenter   = var.datacenter
+  image        = var.image
+  node_type    = var.master_type
+  ssh_keys     = var.ssh_keys
+
+  hcloud_network_id = hcloud_network.private.id
+  hcloud_subnet_id  = hcloud_network_subnet.subnet.id
+
+  k3s_token   = random_string.k3s_token.result
+  k3s_channel = var.k3s_channel
+
+  hcloud_token = var.hcloud_token
+}
+
+module "node_group" {
+  source       = "./modules/node_group"
+  cluster_name = var.cluster_name
+  datacenter   = var.datacenter
+  image        = var.image
+  ssh_keys     = var.ssh_keys
+  master_ipv4  = module.master.master_ipv4
+
+  hcloud_subnet_id = hcloud_network_subnet.subnet.id
+
+  k3s_token   = random_string.k3s_token.result
+  k3s_channel = var.k3s_channel
+
+  for_each   = var.node_groups
+  node_type  = each.key
+  node_count = each.value
+}
+
+module "kubeconfig" {
+  source       = "./modules/kubeconfig"
+  cluster_name = var.cluster_name
+  master_ipv4  = module.master.master_ipv4
+}
+
 output "master_ipv4" {
-  depends_on  = [module.cluster]
+  depends_on  = [module.master]
   description = "Public IP Address of the master node"
-  value       = module.cluster.master_ipv4
+  value       = module.master.master_ipv4
 }
 
 output "nodes_ipv4" {
-  depends_on  = [module.cluster]
-  description = "Public IP Address of the worker nodes"
-  value       = module.cluster.nodes_ipv4
+  depends_on  = [module.node_group]
+  description = "Public IP Address of the worker nodes in groups"
+  value = {
+    for type, n in module.node_group :
+    type => n.node_ipv4
+  }
 }
+
+# # cluster 1
+# module "cluster" {
+#   source       = "cicdteam/k3s/hcloud"
+#   version      = "0.1.2"
+#   hcloud_token = var.hcloud_token
+#   ssh_keys     = ["nuc", "mac"]
+#   datacenter   = "nbg1-dc3"
+#   cluster_name = "k8s"
+#   image        = "ubuntu-20.04"
+#   k3s_channel  = "v1.21"
+#   master_type  = "cx21"
+#   node_groups  = {
+#     "cx21" = 2
+#   }
+# }
+
+# # network output
+# output "master_ipv4" {
+#   depends_on  = [module.cluster]
+#   description = "Public IP Address of the master node"
+#   value       = module.cluster.master_ipv4
+# }
+
+# output "nodes_ipv4" {
+#   depends_on  = [module.cluster]
+#   description = "Public IP Address of the worker nodes"
+#   value       = module.cluster.nodes_ipv4
+# }
 
--- versions.tf
+++ versions.tf
@@ -1,10 +1,34 @@
+# terraform {
+#   required_providers {
+#     hcloud = {
+#       source  = "hetznercloud/hcloud"
+#       version = "1.26.2"
+#     }
+#   }
+#   backend "remote" {
+#     organization = "olender"
+#     workspaces {
+#       name = "terraform-hetzner"
+#     }
+#   }
+# }
+
 terraform {
   required_providers {
     hcloud = {
-      source  = "hetznercloud/hcloud"
-      version = "1.26.2"
+      source = "hetznercloud/hcloud"
+    }
+    local = {
+      source = "hashicorp/local"
+    }
+    random = {
+      source = "hashicorp/random"
+    }
+    template = {
+      source = "hashicorp/template"
     }
   }
+  required_version = ">= 0.12"
   backend "remote" {
     organization = "olender"
     workspaces {
